43acf878c48c1b61b26cc64208fccbbeeffc5e92
Akshat <akshat.shah@go-jek.com>
2016-08-20 19:30:01 +0530

    tested GameBoard class

---
+++ src/main/java/GameBoard.java
@@ -0,0 +1,26 @@
+public class GameBoard {
+
+    private int row = 3, col = 3;
+    private char[][] gameBoard = new char[row][col];
+
+    public boolean checkVacancy(int[] playerInput) {
+        return (gameBoard[playerInput[0]][playerInput[1]] == '_');
+    }
+
+    public boolean checkBoundary(int[] playerInput) {
+        return (playerInput[0] >= 0 && playerInput[0] <= 2 && playerInput[1] >= 0 && playerInput[1] <= 2);
+    }
+
+    public void initializeGameBoard() {
+        for (int ii = 0; ii < row; ii++) {
+            for (int jj = 0; jj < col; jj++) {
+                int[] playerInput = {ii, jj};
+                this.setGameBoard( playerInput, '_');
+            }
+        }
+    }
+
+    public void setGameBoard(int[] playerInput, char gameCharacter) {
+        this.gameBoard[playerInput[0]][playerInput[1]] = gameCharacter;
+    }
+}
---
+++ src/test/java/GameBoardTest.java
@@ -0,0 +1,36 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GameBoardTest {
+
+    GameBoard gameBoard = new GameBoard();
+
+    @Test
+    public void testcheckBoundary1() {
+        gameBoard.initializeGameBoard();
+        int[] playerInput = {2, 1};
+        Assert.assertEquals(gameBoard.checkBoundary(playerInput), true);
+    }
+
+    @Test
+    public void testcheckBoundary2() {
+        gameBoard.initializeGameBoard();
+        int[] playerInput = {2, 4};
+        Assert.assertEquals(gameBoard.checkBoundary(playerInput), false);
+    }
+
+    @Test
+    public void testcheckVacancy1() {
+        gameBoard.initializeGameBoard();
+        int[] playerInput = {2, 1};
+        Assert.assertEquals(gameBoard.checkVacancy(playerInput), true);
+    }
+
+    @Test
+    public void testcheckVacancy2() {
+        gameBoard.initializeGameBoard();
+        int[] playerInput = {2, 1};
+        gameBoard.setGameBoard(playerInput, 'X');
+        Assert.assertEquals(gameBoard.checkVacancy(playerInput), false);
+    }
+}
